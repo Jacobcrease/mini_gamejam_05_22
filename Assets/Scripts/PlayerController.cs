808df17f4640af7bf71b57ab69fb8a220fae28f7
Jakob Knitter <wort993@gmail.com>
2023-05-20 18:13:31 +0200

    change: playercontroller should include wsad

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,43 +8,40 @@ public class PlayerController : MonoBehaviour
     [SerializeField] float rotSpeed = 1.0f;
     private CharacterController characterController;
 
-    private Rigidbody rb;
-
-    public void onMove (Vector2 moveVec)
+    public Vector3 getInput ()
     {
+        float _input_X = 0f;
+        float _input_Y = 0f;
+
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+        {
+            _input_X -= 1;
+        }
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        {
+            _input_X += 1;
+        }
+        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
+        {
+            _input_Y -= 1;
+        }
+        if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
+        {
+            _input_Y += 1;
+        }
+        
+        Vector3 movement = new Vector3(_input_Y * playerSpeed * Time.deltaTime, 0, _input_X * playerSpeed * Time.deltaTime);
+        return movement;
     }
     private void Start()
     {
         characterController = GetComponent<CharacterController>();
     }
-    // Update is called once per frame
+    
     void Update()
     {
-        float _input_X = 0f;
-        float _input_Y = 0f;
+        Vector3 movement = getInput();
 
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            _input_X -= 1;
-        }
-        if (Input.GetKey(KeyCode.RightArrow))
-        {
-            _input_X += 1;
-        }
-        if (Input.GetKey(KeyCode.UpArrow))
-        {
-            _input_Y -= 1;
-        }
-        if (Input.GetKey(KeyCode.DownArrow))
-        {
-            _input_Y += 1;
-        }
-       // _actualSpeed = new Vector2 (_input_X * Time.deltaTime * playerSpeed, _input_Y * Time.deltaTime * playerSpeed);
-        Vector3 movement = new Vector3(_input_Y * playerSpeed * Time.deltaTime, 0, _input_X * playerSpeed * Time.deltaTime);
-      //  movement = transform.TransformDirection(movement);
-
-        Debug.Log(movement);
         characterController.Move(movement);
-      //  transform.position = new Vector3 (transform.position.x + _actualSpeed.x , 0f, transform.position.y + _actualSpeed.y);
     }
 }
