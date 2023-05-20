b0f50385d85d04b6f22bad9d68413ea921533895
Jakob Knitter <wort993@gmail.com>
2023-05-20 18:54:33 +0200

    decrease light via time

---
+++ Assets/Scripts/Managers/LightManager.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class LightManager : MonoBehaviour
+{
+    public static LightManager instance;
+    [SerializeField] private CandleController _candleController;
+    [SerializeField] private float decreaseScaler = 10;
+
+    private void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            instance = this;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        _candleController.SmallDecrease(Time.deltaTime * decreaseScaler);
+    }
+}
