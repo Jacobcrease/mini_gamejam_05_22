2881bbf8b83839923ecc704770ec2b59bfc8334d
Jakob Knitter <wort993@gmail.com>
2023-05-20 18:19:59 +0200

    add candle prefab to player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,27 +10,27 @@ public class PlayerController : MonoBehaviour
 
     public Vector3 getInput ()
     {
-        float _input_X = 0f;
-        float _input_Y = 0f;
+        float _inputLeftRight = 0f;
+        float _inputForwardBackwards = 0f;
 
         if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
         {
-            _input_X -= 1;
+            _inputLeftRight -= 1;
         }
         if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
         {
-            _input_X += 1;
+            _inputLeftRight += 1;
         }
         if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
         {
-            _input_Y -= 1;
+            _inputForwardBackwards -= 1;
         }
         if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
         {
-            _input_Y += 1;
+            _inputForwardBackwards += 1;
         }
         
-        Vector3 movement = new Vector3(_input_Y * playerSpeed * Time.deltaTime, 0, _input_X * playerSpeed * Time.deltaTime);
+        Vector3 movement = new Vector3(_inputForwardBackwards * playerSpeed * Time.deltaTime, 0, _inputLeftRight * playerSpeed * Time.deltaTime);
         return movement;
     }
     private void Start()
